c7a5fe29a431ce378776887d25bb1fcdd9d9244a
DiegoFmDev <150633615+DiegoFmDev@users.noreply.github.com>
2025-07-09 10:57:17 -0400

    Add files via upload
    miercoles 09-07-25

---
+++ UPC-TecnoWEB1/carrouselPeliculas/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.querySelector(".carousel");
+  if (carousel) {
+    console.log("Carousel initialized");
+    // Add carousel functionality here
+  }
+});
